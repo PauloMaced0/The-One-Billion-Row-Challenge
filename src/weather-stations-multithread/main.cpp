9d6a70d9f2fff1a5abe1cd0dadff22f20523a380
PauloMaced0 <paulomacedo@ua.pt>
2025-06-29 00:27:31 +0100

    Use mmap

--- src/weather-stations-multithread/main.cpp
+++ src/weather-stations-multithread/main.cpp
@@ -1,20 +1,24 @@
+#include <chrono>
+#include <cstddef>
+#include <cstdio>
+#include <iomanip>
 #include <stdio.h>
 #include <cstdlib>
-#include <ios>
 #include <iosfwd>
 #include <iostream>
-#include <fstream>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <sys/mman.h>
+#include <sys/fcntl.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <thread>
+#include <unistd.h>
 #include <unordered_map>
-#include "thread_pool.h"
-#include <iomanip> 
 #include <cstring> 
+#include "thread_pool.h"
 
-#define BUFFER_SIZE (1 * 1024 *  1024) // 1 MB
 #define TEMP_MAX 1000
 #define TEMP_MIN -1000
 
@@ -29,12 +33,10 @@ struct WSData {
 
 int parse_float(const char* s) {
     // parse sign
-    int mod;
+    int mod = 1;
     if (*s == '-') {
         mod = -1;
         s++;
-    } else {
-        mod = 1;
     }
 
     if (s[1] == '.') {
@@ -46,20 +48,20 @@ int parse_float(const char* s) {
 
 struct Task {
     std::shared_ptr<std::unordered_map<std::string, WSData>> map;
-    const char* filename;
-    std::streampos start;
-    std::streampos end;
+    const char* data;
+    size_t start;
+    size_t end;
     int tId;
 };
 
-streampos adjustToNextNewline(ifstream &file, streampos pos, streampos fileSize) {
-    file.seekg(pos);
-    char c;
+size_t adjustToNextNewline(const char* data, size_t end, size_t fileSize) {
+    size_t i = end;
 
-    while (file.get(c)) {
-        if (c == '\n' || file.tellg() >= fileSize) {
-            return file.tellg();
+    while (i < fileSize) {
+        if (data[i] == '\n') {
+            return i + 1;
         }
+        i++;
     }
 
     return fileSize;
@@ -67,64 +69,75 @@ streampos adjustToNextNewline(ifstream &file, streampos pos, streampos fileSize)
 
 void FileProcessTask(Task args) 
 {
-    ifstream file(args.filename, std::ios::binary);
-    if (!file) {
-        std::cerr << "Thread " << args.tId << ": Failed to open file " << args.filename << "\n";
-        return;
-    }
+    const char *ptr = args.data + args.start;
+    // const char *end = args.data + args.end;
 
-    char* buffer = (char*) malloc(sizeof(char) * BUFFER_SIZE);
+    uint stringStart = 0;
 
-    file.seekg(args.start);
-
-    uint stringStart;
-    streamsize bytesRead;
-    
     char station[100] = {0};
     char temp[5] = {0};
-    char leftover[100] = {0};
-    char* target;
     int temperature;
 
-    while (file.tellg() < args.end) {
-        // Ensure we don't read past the end
-        bytesRead = min(static_cast<streamsize>(BUFFER_SIZE), streamsize(args.end - file.tellg()));
-    
-        file.read(buffer, bytesRead);
-        bytesRead = file.gcount();  // Get the actual number of bytes read
-    
-        if (bytesRead <= 0) break;  // Stop if nothing was read
-        
-        stringStart = 0;
-        for (uint i = 0; i < bytesRead; i++) {
-            if (buffer[i] == ';' || buffer[i] == '\n') {
-                target = (buffer[i] == ';') ? station : temp;
-                if (leftover[0] == '\0') {
-                    memcpy(target, buffer + stringStart, i - stringStart);
-                    target[i - stringStart] = '\0';
-                } else {
-                    strncat(leftover, buffer + stringStart, i - stringStart);
-                    strcpy(target, leftover);
-                    leftover[0] = '\0';
-                }
-                stringStart = i + 1;
-                if (buffer[i] == '\n') {
-                    temperature = parse_float(temp);
+    // const char *sep;
+    // const char *newline;
 
-                    WSData& entry = (*args.map)[station];
-                    entry.cnt += 1;
-                    entry.sum += temperature;
-                    entry.max = max(entry.max, temperature);
-                    entry.min = min(entry.min, temperature);
-                }
-            }
+    auto startclk = chrono::high_resolution_clock::now();
+
+    // while (ptr < end) {
+    //     // Find ';'
+    //     sep = ptr;
+    //     while (sep < end && *sep != ';') ++sep;
+    // 
+    //     memcpy(station, ptr, sep - ptr);
+    //     station[sep - ptr] = '\0';
+    // 
+    //     // Advance past ';'
+    //     if (sep < end) ++sep;
+    // 
+    //     // Find '\n'
+    //     newline = sep;
+    //     while (newline < end && *newline != '\n') ++newline;
+    // 
+    //     memcpy(temp, sep, newline - sep);
+    //     temp[newline - sep] = '\0';
+    // 
+    //     temperature = parse_float(temp);
+    // 
+    //     auto &entry = (*args.map)[station];
+    //     entry.cnt += 1;
+    //     entry.sum += temperature;
+    //     entry.max = max(entry.max, temperature);
+    //     entry.min = min(entry.min, temperature);
+    // 
+    //     // Move to next line
+    //     if (newline < end) ++newline;
+    //     ptr = newline;
+    // }
+    
+    for (size_t i = 0; i < args.end - args.start; i++) {
+        if (ptr[i] == ';') {
+            memcpy(station, ptr + stringStart, i - stringStart);
+            station[i - stringStart] = '\0';
+            stringStart = i + 1;
+        } else if (ptr[i] == '\n') {
+            memcpy(temp, ptr + stringStart, i - stringStart);
+            temp[i - stringStart] = '\0';
+
+            temperature = parse_float(temp);
+
+            WSData& entry = (*args.map)[station];
+            entry.cnt += 1;
+            entry.sum += temperature;
+            entry.max = max(entry.max, temperature);
+            entry.min = min(entry.min, temperature);
+
+            stringStart = i + 1;
         }
-        strncpy(leftover, buffer + stringStart, bytesRead - stringStart);
-        leftover[bytesRead - stringStart] = '\0';
     }
 
-    free(buffer);
-    file.close();
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - startclk);
+    cout << "Task creation and execution takes: "<< duration.count() << " ms"<< endl;
 
     return;
 }
@@ -181,42 +194,47 @@ int main(int argc, char* argv[])
 
     ThreadPool thread_pool(numThreads);
 
-    // consider this if multiple input file is implemented
-    // int splitFactor = (1 << number of input file) * numThreads;
-    int splitFactor = 2 * numThreads;
-
-    ifstream fh(file, ios::binary | ios::ate);
-    if (not fh.is_open()){
+    int fd = open(file, O_RDONLY); 
+    if (fd == -1){
         std::cerr << "Unable to open '" << file << "'" << std::endl;
         return 1;
     }
     
     vector<std::shared_ptr<std::unordered_map<string, WSData>>> stats;
 
-    streampos fileSize = fh.tellg();
-    fh.seekg(0, ios::beg);
+    struct stat sb;
+    if (fstat(fd, &sb) == -1) {
+        std::cerr << "fstat failed." << std::endl;
+        return 1;
+    }
 
-    // change 20 to a dynamic value (this is the file slipt factor) 
-    streampos chunkSize = fileSize / splitFactor;
+    size_t fileSize = (size_t)sb.st_size;
 
-    streampos start = 0;
-    streampos end = chunkSize;
+    char *mapped = (char*) mmap(NULL, fileSize, PROT_READ, MAP_SHARED, fd, 0);
+    if (mapped == MAP_FAILED) {
+        std::cerr << "Unable to map '" << file << "'" << std::endl;
+        return 1;
+    }
 
+    int splitFactor = 10 * numThreads;
+    size_t chunkSize = fileSize / splitFactor;
+
+    size_t start = 0;
+    size_t end = chunkSize;
+
+    // Task creation and execution is the bottleneck
     for (int i = 0; i < splitFactor; i++) {
         // Open file to adjust the end position to the next newline
-        end = adjustToNextNewline(fh, end, fileSize);
+        end = adjustToNextNewline(mapped, end, fileSize);
 
         auto map = std::make_shared<std::unordered_map<string, WSData>>();
-
-        // Launch thread
         stats.push_back(map);
-        Task task {map, file, start, end, i};
-        thread_pool.enqueue([task]() {
-            FileProcessTask(task);
-        });
 
-        start = end;  // Next thread starts from this position
+        // Launch task
+        Task task {map, mapped, start, end, i};
+        thread_pool.enqueue([task]() { FileProcessTask(task); });
 
+        start = end;  // Next task starts from this position
         end = (i == splitFactor - 2) ? fileSize : start + chunkSize;
     }
 
@@ -250,7 +268,7 @@ int main(int argc, char* argv[])
         } 
         thread_pool.wait();
     }
-
+    
     for(const auto &item : statsVector) {
         cout << item.first 
             << ": avg=" << fixed << setprecision(1) << (float)item.second.sum/ (10 * item.second.cnt)
@@ -259,5 +277,6 @@ int main(int argc, char* argv[])
             << "\n";
     }
 
-    fh.close();
+    munmap(mapped, fileSize);
+    close(fd);
 }
